5ba2503fc03a1797258a7fd08b429585a1b2ad91
terence.chung <b195fea@gmail.com>
2020-08-05 01:10:07 +0800

    爬蟲更正，標籤下再取標籤。

--- idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/items.py
+++ idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/items.py
@@ -8,14 +8,14 @@ import scrapy
 class MyfirstscrapyprojectItem(scrapy.Item):
     # define the fields for your item here like:
     C00_title = scrapy.Field()
-    C03_floor = scrapy.Field()
-    C02_authorName = scrapy.Field()
     C01_authorId = scrapy.Field()
+    C02_authorName = scrapy.Field()
+    C03_floor = scrapy.Field()
     C04_time = scrapy.Field()
     C05_content = scrapy.Field()
-    C10_NORMAL = scrapy.Field()
-    C08_FIRST_LETTER = scrapy.Field()
-    C09_INITIALS = scrapy.Field()
-    C07_BOPOMOFO = scrapy.Field()
-    C06_BOPOMOFO_FIRST = scrapy.Field()
+    # C10_NORMAL = scrapy.Field()
+    # C08_FIRST_LETTER = scrapy.Field()
+    # C09_INITIALS = scrapy.Field()
+    # C07_BOPOMOFO = scrapy.Field()
+    # C06_BOPOMOFO_FIRST = scrapy.Field()
 
--- idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/spiders/scrapyTest.py
+++ idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/spiders/scrapyTest.py
@@ -18,12 +18,12 @@ class gammerSpider(scrapy.Spider):
     # 標籤頁
     def parse(self, response):
         # urls = response.css('a[class=b-list__main__title]').extract()
-
         # 單頁面測試
         # next_page_url = response.xpath('//a[@class="b-list__main__title"]/@href').extract_first()
         # url = response.urljoin(next_page_url)
         # if url:
         #     yield scrapy.Request(url, callback=self.parseContent)
+        #     # yield scrapy.Request(url, callback=self.parseContent)
 
         # 全部爬蟲
         # 進入文章內容
@@ -42,70 +42,48 @@ class gammerSpider(scrapy.Spider):
                 yield scrapy.Request(url, callback=self.parseContent)
 
         # 文章列表 下一頁
-        # next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
-        # url = response.urljoin(next_page_url)
-        # if url:
-        #     yield scrapy.Request(url, callback=self.parse)
+        next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
+        url = response.urljoin(next_page_url)
+        if url:
+            yield scrapy.Request(url, callback=self.parse)
 
-    # 抓取頁面內容
     def parseContent(self, response):
+        # 寫入檔案
         # print(response.body)
         # page = response.url.split("/")[-2]
         # filename = 'quotes-%s.html' % page
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
 
-        # items = MyfirstscrapyprojectItem
+        try:
+            url = response.url
+            title = response.selector.xpath('//h1[@class="title"]/text()').get()
+            xpath_post = response.selector.xpath('//div[@class="c-section__main c-post "]')
+            for selctorItem in xpath_post:
+                floor = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[1]/text()').get()
+                authorName = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[2]/text()').get()
+                authorId = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[3]/text()').get()
+                time = selctorItem.xpath('.//div[@class="c-post__header__info"]/*/text()').get()[0:19]
+                content = selctorItem.xpath('.//div[@class="c-article__content"]').xpath('string(.)').extract()[0]
+                item = MyfirstscrapyprojectItem()
+                item['C00_title'] = title
+                item['C01_authorId'] = authorId
+                item['C02_authorName'] = authorName
+                item['C03_floor'] = floor
+                item['C04_time'] = time
+                item['C05_content'] = content
+                # item['C06_BOPOMOFO_FIRST'] = pinyin(contentData, style=Style.BOPOMOFO_FIRST)
+                # item['C07_BOPOMOFO'] = pinyin(contentData, style=Style.BOPOMOFO)
+                # item['C08_FIRST_LETTER'] = pinyin(contentData, style=Style.FIRST_LETTER)
+                # item['C09_INITIALS'] = pinyin(contentData, style=Style.INITIALS)
+                # item['C10_NORMAL'] = pinyin(contentData, style=Style.NORMAL)
+                yield item
+        except Exception:
+            print("發生錯誤")
 
-        # 取得標題
-        title = response.selector.xpath('//h1[@class="title"]/text()').get()
-        # 取得內文
-        # content = response.selector.xpath('//div[@class="c-article__content"]/*/text()').getall()
-        content = response.selector.xpath('//div[@class="c-article__content"]')
-        # 取得作者
-        # 樓主
-        floor = response.selector.xpath('//div[@class="c-post__header__author"]/a[1]/text()').getall()
-        # 作者中文
-        authorName = response.selector.xpath('//div[@class="c-post__header__author"]/a[2]/text()').getall()
-        # 作者ID
-        authorId = response.selector.xpath('//div[@class="c-post__header__author"]/a[3]/text()').getall()
-        # 取得日期時間
-        time = response.selector.xpath('//div[@class="c-post__header__info"]/*/text()').getall()
-        print(title)
-
-        # items['title'] = title
-        items = []
-        print(title)
-        for index in range(len(authorId)):
-            # print(str(index) + "-----------------------")
-            # print(floor[index])
-            # print(authorName[index])
-            # print(authorId[index])
-            # print(time[index])
-            # print('message：' + content[index].xpath('string(.)').extract()[0])
-            item = MyfirstscrapyprojectItem()
-
-            contentData = content[index].xpath('string(.)').extract()[0]
-            item['C10_NORMAL'] = pinyin(contentData, style=Style.NORMAL)
-            item['C08_FIRST_LETTER'] = pinyin(contentData, style=Style.FIRST_LETTER)
-            item['C09_INITIALS'] = pinyin(contentData, style=Style.INITIALS)
-            item['C07_BOPOMOFO'] = pinyin(contentData, style=Style.BOPOMOFO)
-            item['C06_BOPOMOFO_FIRST'] = pinyin(contentData, style=Style.BOPOMOFO_FIRST)
-            item['C05_content'] = contentData
-            item['C04_time'] = time[index]
-            item['C03_floor'] = floor[index]
-            item['C02_authorName'] = authorName[index]
-            item['C01_authorId'] = authorId[index]
-            item['C00_title'] = title
-            yield item
-            # items.append(item)
-
-        # return items
         # 下一頁
         #
         next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
         url = response.urljoin(next_page_url)
         if url:
             yield scrapy.Request(url, callback=self.parseContent)
-
-
