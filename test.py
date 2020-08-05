5fcbda70b45f00c5e39f207a32e49fb2f665b8c7
b195fea <b195fea@gmail.com>
2020-08-05 17:26:46 +0800

    增加selenium測試程式碼

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-from pypinyin import pinyin, lazy_pinyin, Style
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-a = pinyin('中心', heteronym=True)
-b = pinyin('中心', style=Style.BOPOMOFO)  # 注音风格
-print(b)
+try:
+    # 啟動無頭模式
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # 規避google bug
+    chrome_options.add_argument('--disable-gpu')
+    # Chrome 驅動程式
+    executable_path = 'D:\\9.workspace\\02.PYTHON\\chromedriver.exe'
+    # 啟動
+    driver = webdriver.Chrome(executable_path=executable_path,
+                              chrome_options=chrome_options)
+    # 設置等待時間
+    driver.implicitly_wait(30)
+
+    url = 'https://forum.gamer.com.tw/C.php?bsn=31066&snA=1510&tnum=1386'
+    driver.get(url)
+    a_href = driver.find_element_by_xpath('//div[@class="c-reply__head nocontent"]/a')
+    print(a_href)
+    a_href.click()
+except:
+    print("發生錯誤")
