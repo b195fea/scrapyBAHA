277d8edf41233c2369334c50a1174915b49ac4ce
terence.chung <b195fea@gmail.com>
2020-08-10 00:33:51 +0800

    爬蟲更正完畢，已經可以爬取所有資料。

--- idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/spiders/scrapyTest.py
+++ idv/zjh/test/myFirstScrapyProject/myFirstScrapyProject/spiders/scrapyTest.py
@@ -2,9 +2,10 @@ import scrapy
 
 from scrapy import Selector
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from idv.zjh.test.myFirstScrapyProject.myFirstScrapyProject.items import MyfirstscrapyprojectItem
-from pypinyin import pinyin, lazy_pinyin, Style
 from scrapy.http import Request, FormRequest
+from pypinyin import pinyin, lazy_pinyin, Style
 
 class gammerSpider(scrapy.Spider):
     name = "bh3"
@@ -13,39 +14,49 @@ class gammerSpider(scrapy.Spider):
     start_urls = ('https://forum.gamer.com.tw/B.php?bsn=31066',)
 
     def __init__(self):
+        executable_path = 'D:\\driver\\chromedriver.exe'
         self.driver = webdriver.chrome
+        # 啟動無頭模式
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 規避google bug
+        chrome_options.add_argument('--disable-gpu')
+        # driver = webdriver.Chrome(executable_path=executable_path,chrome_options=chrome_options)
+        self.browser = webdriver.Chrome(executable_path=executable_path,chrome_options=chrome_options)
+        # self.browser = webdriver.Chrome(executable_path=executable_path)
+        # 設置等待時間
+        # self.driver.implicitly_wait(30)
 
     # 標籤頁
     def parse(self, response):
         # urls = response.css('a[class=b-list__main__title]').extract()
         # 單頁面測試
-        next_page_url = response.xpath('//a[@class="b-list__main__title"]/@href').extract_first()
-        url = response.urljoin(next_page_url)
-        if url:
-            yield scrapy.Request(url, callback=self.parseContent)
-            # yield scrapy.Request(url, callback=self.parseContent)
+        # next_page_url = response.xpath('//a[@class="b-list__main__title"]/@href').extract_first()
+        # url = response.urljoin(next_page_url)
+        # if url:
+        #     yield scrapy.Request(url, callback=self.parseContent)
+        #     # yield scrapy.Request(url, callback=self.parseContent)
 
         # 全部爬蟲
         # 進入文章內容
-        # next_page_url = response.xpath('//a[@class="b-list__main__title"]/@href').extract()
-        # for next in next_page_url:
-        #     url = response.urljoin(next)
-        #     # print(url)
-        #     if url:
-        #         yield scrapy.Request(url, callback=self.parseContent)
-        #
-        # next_page_url = response.xpath('//p[@class="b-list__main__title"]/@href').extract()
-        # for next in next_page_url:
-        #     url = response.urljoin(next)
-        #     # print(url)
-        #     if url:
-        #         yield scrapy.Request(url, callback=self.parseContent)
+        next_page_url = response.xpath('//a[@class="b-list__main__title"]/@href').extract()
+        for next in next_page_url:
+            url = response.urljoin(next)
+            # print(url)
+            if url:
+                yield scrapy.Request(url, callback=self.parseContent)
+
+        next_page_url = response.xpath('//p[@class="b-list__main__title"]/@href').extract()
+        for next in next_page_url:
+            url = response.urljoin(next)
+            # print(url)
+            if url:
+                yield scrapy.Request(url, callback=self.parseContent)
 
         # 文章列表 下一頁
-        # next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
-        # url = response.urljoin(next_page_url)
-        # if url:
-        #     yield scrapy.Request(url, callback=self.parse)
+        next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
+        url = response.urljoin(next_page_url)
+        if url:
+            yield scrapy.Request(url, callback=self.parse)
 
     def parseContent(self, response):
         # 寫入檔案
@@ -56,11 +67,30 @@ class gammerSpider(scrapy.Spider):
         #     f.write(response.body)
 
         try:
-            url = response.url
-            title = response.selector.xpath('//h1[@class="title"]/text()').get()
-            xpath_post = response.selector.xpath('//div[@class="c-section__main c-post "]')
+            browser = self.browser
+            # 設定URL
+            browser.get(response.url)
+            # 取得打開後的網址內容
+            sel = Selector(text=browser.page_source)
+            # 對所有留言點擊打開
+            a_href = browser.find_elements_by_css_selector('a.more-reply')
+            for a in a_href:
+                browser.execute_script("arguments[0].click();", a)
 
+            pageSource = browser.page_source
+            print(pageSource)
+
+            # 取得打開後的網址內容
+            # sel = Selector(text=self.browser.page_source)
+            # title = response.selector.xpath('//h1[@class="title"]/text()').get()
+            # xpath_post = response.selector.xpath('//div[@class="c-section__main c-post "]')
+
+            # 取得所有頁面資訊
+            title = sel.xpath('//h1[@class="title"]/text()').get()
+            xpath_post = sel.xpath('//div[@class="c-section__main c-post "]')
+            #留言第一層
             for selctorItem in xpath_post:
+
                 floor = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[1]/text()').get()
                 authorName = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[2]/text()').get()
                 authorId = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[3]/text()').get()
@@ -79,12 +109,31 @@ class gammerSpider(scrapy.Spider):
                 # item['C09_INITIALS'] = pinyin(contentData, style=Style.INITIALS)
                 # item['C10_NORMAL'] = pinyin(contentData, style=Style.NORMAL)
                 yield item
-        except Exception:
-            print("發生錯誤")
 
+            # 第二層留言
+            for selctorItem in xpath_post:
+                reply_post = selctorItem.xpath('.//div[@class="c-reply__item"]')
+                if len(reply_post) != 0:
+                    floor = selctorItem.xpath('.//div[@class="c-post__header__author"]/a[1]/text()').get()
+                    for replyItem in reply_post:
+                        authorName = replyItem.xpath('.//a[@class="reply-content__user"]/text()').get()
+                        authorId = replyItem.xpath('.//a[@class="reply-content__user"]/@href').get()[20:]
+                        time = replyItem.xpath('.//div[@class="edittime"]/text()').get()[0:19]
+                        content = replyItem.xpath('.//article[@class="reply-content__article c-article "]').xpath('string(.)').extract()[0]
+                        item = MyfirstscrapyprojectItem()
+                        item['C00_title'] = title
+                        item['C01_authorId'] = authorId
+                        item['C02_authorName'] = authorName
+                        item['C03_floor'] = floor
+                        item['C04_time'] = time
+                        item['C05_content'] = content
+                        yield item
+
+        except Exception as e:
+            print("發生錯誤：" + e)
         # 下一頁
         #
-        # next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
-        # url = response.urljoin(next_page_url)
-        # if url:
-        #     yield scrapy.Request(url, callback=self.parseContent)
+        next_page_url = response.xpath('//a[@class="next"]/@href').extract_first()
+        url = response.urljoin(next_page_url)
+        if url:
+            yield scrapy.Request(url, callback=self.parseContent)
--- idv/zjh/test/myFirstScrapyProject/runScrapy.py
+++ idv/zjh/test/myFirstScrapyProject/runScrapy.py
@@ -3,6 +3,6 @@ from scrapy import cmdline
 
 if __name__ == '__main__':
     # cmdline.execute("scrapy crawl bh3".split())
-    cmdline.execute("scrapy crawl bh3 -o bh3-2020-08-04.csv".split())
+    cmdline.execute("scrapy crawl bh3 -o bh3-2020-08-10.csv".split())
     # subprocess.run('scrapy crawl bh3')
     # print(t.getName())
